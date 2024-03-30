5979ce9deae945f23ba444176a3035ea1a9bd952
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2024-03-30 21:52:28 +0200

    Refactor flasher to move everything to a class

--- jn51xx_flasher.py
+++ jn51xx_flasher.py
@@ -3,6 +3,7 @@ import struct
 import argparse
 import socket
 
+from jn51xx_protocol import *
 
 class Uart2SocketWrapper:
     def __init__(self, sock):
@@ -20,231 +21,274 @@ def check(cond, errmsg):
         raise RuntimeError(errmsg)
 
 
-def trace(msg):
-    enabled = False
-    if enabled:
-        print(msg)
+class Flasher:
+    """ Class to handle communication with JN5169 device """
+
+    def __init__(self, ser, verbose="none"):
+        self.ser = ser
+        self.verbose = verbose
 
 
-def calcCRC(data):
-    res = 0
-    for b in data:
-        res ^= b
-    
-    return res
+    def sendRequest(self, msgtype, data):
+        """ Send a request to the device and return the response"""
 
-def sendRequest(ser, msgtype, data):
-    # Prepare the message
-    msg = struct.pack("<BB", len(data) + 2, msgtype)
-    msg += data
-    msg += calcCRC(msg  ).to_bytes(1, 'big')
-    
-    # Send the message
-    trace("Sending: " + ' '.join('{:02x}'.format(x) for x in msg))
-    ser.write(msg)
+        # Prepare the message
+        msglen = len(data) + 2
+        msg = struct.pack("<BB", msglen, msgtype)
+        msg += data
+        msg += calcCRC(msg).to_bytes(1, 'big')
+        
+        # Dump the request to send
+        if self.verbose != "none":
+            dumpMessage(">", msglen, msgtype, msg[2:], self.verbose == "raw")
 
-    # Wait for response
-    data = ser.read(2)
-    check(data, "No response from device")
+        # Send the request
+        self.ser.write(msg)
 
-    resplen, resptype = struct.unpack('BB', data)
-    trace("Response type={:02x} length={}".format(resptype, resplen))
-    data = ser.read(resplen - 1)
-    check(data, "Incorrect response from device")
+        # Wait for response
+        data = self.ser.read(2)
+        check(data, "No response from device")
 
-    trace("Received: " + "{:02x} {:02x} ".format(resplen, resptype) + ' '.join('{:02x}'.format(x) for x in data))
-    check(msgtype + 1 == resptype, "Incorrect response type")   # Looks like request and response type numbers are next to each other
+        # Parse the response header, and wait for the rest of data
+        resplen, resptype = struct.unpack('BB', data)
+        data = self.ser.read(resplen - 1)
+        check(data, "Incorrect response from device")
+        check(msgtype + 1 == resptype, "Incorrect response type")   # Looks like request and response type numbers are next to each other
 
-    return data[:-1]
-
-
-def getChipId(ser):
-    print("Requesting Chip ID")
-    resp = sendRequest(ser, 0x32, b'')
-
-    bootloaderVer = None
-    if len(resp) == 5:
-        status, chipId = struct.unpack('>BI', resp)
-    else:
-        status, chipId, bootloaderVer = struct.unpack('>BII', resp)
-
-    print("Received chip ID {:08x}, Bootloader={:08x} (Status={:02x})".format(chipId, bootloaderVer, status))
-
-    # Chip ID structure
-    #define CHIP_ID_MANUFACTURER_ID_MASK    0x00000fff
-    #define CHIP_ID_PART_MASK               0x003ff000
-    #define CHIP_ID_MASK_VERSION_MASK       0x0fc00000
-    #define CHIP_ID_REV_MASK                0xf0000000
-
-    check(status == 0, "Wrong status on get Chip ID request")
-    check(chipId & 0x003fffff == 0x0000b686, "Unsupported chip ID")   # Support only JN5169 for now
-    return chipId
+        # Dump the response
+        if self.verbose != "none":
+            dumpMessage("<", resplen, resptype, data, self.verbose == "raw")
+            
+        # Return the response payload
+        return data[:-1]
 
 
-def setFlashType(ser):
-    print("Selecting internal flash")
-    req = struct.pack("<BI", 8, 0x00000000) # Select internal flash (8) at addr 0x00000000
-    resp = sendRequest(ser, 0x2c, req)
-    status = struct.unpack("<B", resp)
-    check(status[0] == 0, "Wrong status on select internal flash")
+    def getChipId(self):
+        """ Get the chip ID of the device, verify it is JN5169 """
+
+        resp = self.sendRequest(GET_CHIP_ID_REQUEST, b'')
+
+        # Parse response
+        bootloaderVer = None
+        if len(resp) == 5:
+            status, chipId = struct.unpack('>BI', resp)
+        else:
+            status, chipId, bootloaderVer = struct.unpack('>BII', resp)
+
+        print(f"Chip ID: {chipId:08x}, Bootloader={bootloaderVer:08x} (Status={status:02x})")
+
+        # Chip ID structure
+        #define CHIP_ID_MANUFACTURER_ID_MASK    0x00000fff
+        #define CHIP_ID_PART_MASK               0x003ff000
+        #define CHIP_ID_MASK_VERSION_MASK       0x0fc00000
+        #define CHIP_ID_REV_MASK                0xf0000000
+
+        check(status == 0, "Wrong status on get Chip ID request")
+        check(chipId & 0x003fffff == 0x0000b686, "Unsupported chip ID")   # Support only JN5169 for now
+        return chipId
 
 
-def readRAM(ser, addr, len):
-    req = struct.pack("<IH", addr, len)
-    resp = sendRequest(ser, 0x1f, req)
-    check(resp[0] == 0, "Wrong status on read RAM request")
-    return [x for x in resp[1:1+len]]
+    def selectFlashType(self, flashType = 8):
+        """ Select the flash type to use. By default select internal flash (8) """
+
+        print("Selecting internal flash")
+        req = struct.pack("<BI", flashType, 0x00000000)
+
+        resp = self.sendRequest(SELECT_FLASH_TYPE_REQUEST, req)
+        status = struct.unpack("<B", resp)
+        check(status[0] == 0, "Wrong status on select internal flash request")
 
 
-def writeRAM(ser, addr, data):
-    req = struct.pack("<I", addr)
-    req += data
-    resp = sendRequest(ser, 0x1d, req)
-    check(resp[0] == 0, "Wrong status on read RAM request")
+    def readRAM(self, addr, len):
+        """ Read data from RAM at the given address """
+
+        req = struct.pack("<IH", addr, len)
+        resp = self.sendRequest(RAM_READ_REQUEST, req)
+        check(resp[0] == 0, "Wrong status on read RAM request")
+        return [x for x in resp[1:1+len]]
 
 
-def getSettings(ser):
-    print("Requesting device customer settings")
-    settings = readRAM(ser, 0x01001510, 16)
-    print("Device settings: " + ':'.join('{:02x}'.format(x) for x in settings))
-    return settings
+    def writeRAM(self, addr, data):
+        """ Write data to RAM at the given address """
+
+        req = struct.pack("<I", addr)
+        req += data
+        resp = self.sendRequest(RAM_WRITE_REQUEST, req)
+        check(resp[0] == 0, "Wrong status on read RAM request")
 
 
-def resetSettings(ser):
-    print("Resetting device customer settings")
-    writeRAM(ser, 0x01001510, b'\xff')
+    def getChipSettings(self):
+        """ Get the chip settings bytes """
+
+        settings = self.readRAM(CHIP_SETTINGS_ADDRESS, 16)
+        print("Device settings: " + ':'.join(f'{x:02x}' for x in settings))
+        return settings
 
 
-def getUserMAC(ser):
-    print("Requesting device User MAC address")
-    mac = readRAM(ser, 0x01001570, 8)
-    print("Device User MAC address: " + ':'.join('{:02x}'.format(x) for x in mac))
-    return mac
+    def getUserMAC(self):
+        """ Get the user MAC address of the device """
+
+        mac = self.readRAM(OVERRIDEN_MAC_ADDRESS, 8)
+        print("Device User MAC address: " + ':'.join(f'{x:02x}' for x in mac))
+        return mac
 
 
-def getFactoryMAC(ser):
-    print("Requesting device Factory MAC address")
-    mac = readRAM(ser, 0x01001580, 8)
-    print("Device Factory MAC address: " + ':'.join('{:02x}'.format(x) for x in mac))
-    return mac
+    def getFactoryMAC(self):
+        """ Get the factory MAC address of the device """
+        
+        mac = self.readRAM(FACTORY_MAC_ADDRESS, 8)
+        print("Device Factory MAC address: " + ':'.join(f'{x:02x}' for x in mac))
+        return mac
 
 
-def getMAC(ser):
-    mac = getUserMAC(ser)
-    if mac == [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]:
-        mac = getFactoryMAC(ser)
-    return mac
+    def getMAC(self):
+        mac = self.getUserMAC()
+        if mac == [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]:
+            mac = self.getFactoryMAC()
+        return mac
 
 
-def eraseFlash(ser):
-    print("Erasing internal flash")
-    resp = sendRequest(ser, 0x07, b'')
-    status = struct.unpack("<B", resp)
-    check(status[0] == 0, "Wrong status on erase internal flash")
+    def eraseFlash(self):
+        """ Erase the microcontroller flash memory """
+
+        print("Erasing internal flash")
+        resp = self.sendRequest(FLASH_ERASE_REQUEST, b'')
+        status = struct.unpack("<B", resp)
+        check(status[0] == 0, "Wrong status on erase internal flash")
 
 
-def reset(ser):
-    print("Reset target device")
-    resp = sendRequest(ser, 0x14, b'')
-    status = struct.unpack("<B", resp)
-    check(status[0] == 0, "Wrong status on reset device")
+    def reset(self):
+        """ Reset the target micocontroller """
+
+        print("Reset target device")
+        resp = self.sendRequest(RESET_REQUEST, b'')
+        status = struct.unpack("<B", resp)
+        check(status[0] == 0, "Wrong status on reset device")
 
 
-def writeFlash(ser, addr, chunk):
-    print("Writing flash at addr {:08x}".format(addr))
-    req = struct.pack("<I", addr)
-    req += chunk
-    resp = sendRequest(ser, 0x09, req)
+    def writeFlash(self, addr, chunk):
+        """ Write flash data at the given address """
 
-    check(resp[0] == 0, "Wrong status on write flash command")
-
-
-def readFlash(ser, addr, len):
-    print("Reading flash at addr {:08x}".format(addr))
-    req = struct.pack("<IH", addr, len)
-    resp = sendRequest(ser, 0x0b, req)
-    check(resp[0] == 0, "Wrong status on read flash request")
-    return resp[1:1+len]
+        print(f"Writing flash at addr {addr:08x}")
+        req = struct.pack("<I", addr)
+        req += chunk
+        resp = self.sendRequest(FLASH_WRITE_REQUEST, req)
+        check(resp[0] == 0, "Wrong status on write flash command")
 
 
-def loadFirmwareFile(filename):
-    # Load a file to flash
-    with open(filename, "rb") as f:
-        firmware = f.read()
-    check(firmware[0:4] == b'\x0f\x03\x00\x0b', "Incorrect firmware format")
-    firmware = firmware[4:]
-    return firmware
+    def readFlash(self, addr, len):
+        """ Read flash data at the given address """
+
+        print(f"Reading flash at addr {addr:08x}")
+        req = struct.pack("<IH", addr, len)
+        resp = self.sendRequest(FLASH_READ_REQUEST, req)
+        check(resp[0] == 0, "Wrong status on read flash request")
+        return resp[1:1+len]
 
 
-def saveFirmwareFile(filename, content):
-    # Load a file to flash
-    with open(filename, "w+b") as f:
-        #f.write(b'\x0f\x03\x00\x0b')
-        f.write(content)
+    def loadFirmwareFile(self, filename):
+        """ Load the firmware file """
+
+        # Load the file data
+        with open(filename, "rb") as f:
+            firmware = f.read()
+        
+        # Strip the file type marker
+        check(firmware[0:4] == b'\x0f\x03\x00\x0b', "Incorrect firmware format")
+        firmware = firmware[4:]
+
+        return firmware
 
 
-def writeFirmware(ser, filename):
-    firmware = loadFirmwareFile(filename)
+    def saveFirmwareFile(self, filename, content):
+        """ Save the data to the file """
 
-    # Prepare flash
-    setFlashType(ser)
-    eraseFlash(ser)
-    #resetSettings(ser)
-
-    # Flash data
-    for addr in range(0, len(firmware), 0x80):
-        chunklen = len(firmware) - addr
-        if chunklen > 0x80:
-            chunklen = 0x80
-
-        writeFlash(ser, addr, firmware[addr:addr+chunklen])
-
-    # Finalize and reset the device into the firmware
-    reset(ser)
+        # Load a file to flash
+        with open(filename, "w+b") as f:
+            f.write(b'\x0f\x03\x00\x0b')
+            f.write(content)
 
 
-def verifyFirmware(ser, filename):
-    firmware = loadFirmwareFile(filename)
+    def writeFirmware(self, filename):
+        """" Write the firmware to the device """
 
-    # Prepare the flash
-    setFlashType(ser)
+        firmware = self.loadFirmwareFile(filename)
 
-    # Verify flash data
-    errors = False
-    for addr in range(0, len(firmware), 0x80):
-        chunklen = len(firmware) - addr
-        if chunklen > 0x80:
-            chunklen = 0x80
+        # Prepare flash
+        self.selectFlashType()
+        self.eraseFlash()
 
-        chunk = readFlash(ser, addr, chunklen)
+        # Flash data
+        for addr in range(0, len(firmware), 0x80):
+            chunklen = len(firmware) - addr
+            if chunklen > 0x80:
+                chunklen = 0x80
 
-        if chunk != firmware[addr:addr+chunklen]:
-            print("Firmware verification failed: data different at addr {:08x}".format(addr))
-            errors = True
+            self.writeFlash(addr, firmware[addr:addr+chunklen])
 
-    print("Firmware verification " + ("failed" if errors else "successful"))
-
-    # Finalize and reset the device into the firmware
-    reset(ser)
+        # Finalize and reset the device into the firmware
+        self.reset()
 
 
-def readFirmware(ser, filename):
+    def verifyFirmware(self, filename):
+        """ Verify the firmware on the device against the given file """
 
-    getSettings(ser)
+        firmware = self.loadFirmwareFile(filename)
 
-    # Prepare flash
-    setFlashType(ser)
+        # Prepare the flash
+        self.selectFlashType()
 
-    # Flash data
-    firmware = b''
-    for addr in range(0, 512*1024, 0x80):
-        firmware += readFlash(ser, addr, 0x80)
+        # Verify flash data
+        errors = False
+        for addr in range(0, len(firmware), 0x80):
+            chunklen = len(firmware) - addr
+            if chunklen > 0x80:
+                chunklen = 0x80
 
-    # Finalize and reset the device into the firmware
-    reset(ser)
+            chunk = self.readFlash(addr, chunklen)
 
-    # Save downloaded firmware content
-    saveFirmwareFile(filename, firmware)
+            if chunk != firmware[addr:addr+chunklen]:
+                print(f"Firmware verification failed: data different at addr {addr:08x}")
+                errors = True
+
+        print("Firmware verification " + ("failed" if errors else "successful"))
+
+        # Finalize and reset the device into the firmware
+        self.reset()
+
+
+    def readFirmware(self, filename):
+        """ Read the firmware from the device """
+
+        self.getSettings()
+
+        # Prepare flash
+        self.setFlashType()
+
+        # Flash data
+        firmware = b''
+        for addr in range(0, 512*1024, 0x80):
+            firmware += self.readFlash(addr, 0x80)
+
+        # Finalize and reset the device into the firmware
+        self.reset()
+
+        # Save downloaded firmware content
+        self.saveFirmwareFile(filename, firmware)
+
+
+    def run(self, action, filename):
+        """ Perform the requested action on the device """
+
+        # Prepare the target device
+        self.getChipId()
+        mac = self.getMAC()
+        print("Effective device MAC address: " + ':'.join(f'{x:02x}' for x in mac))
+
+        match action:
+            case "write": self.writeFirmware(filename)
+            case "read": self.readFirmware(filename)
+            case "verify": self.verifyFirmware(filename)
 
 
 def main():
@@ -252,6 +296,7 @@ def main():
     parser = argparse.ArgumentParser(description="Flash NXP JN5169 device")
     parser.add_argument("-p", "--port", help="Serial port")
     parser.add_argument("-s", "--server", help="Remote flashing server")
+    parser.add_argument("-v", "--verbose", nargs='?', choices=["none", "protocol", "raw"], help="Set verbosity level", default="none")
     parser.add_argument("action", choices=["read", "write", "verify"], help="Action to perform: read, write, verify")
     parser.add_argument("file", help="Firmware file to flash")
     args = parser.parse_args()
@@ -273,17 +318,10 @@ def main():
         sock.connect((args.server, 5169))
         ser = Uart2SocketWrapper(sock)
 
-    # Prepare the target device
-    getChipId(ser)
-    mac = getMAC(ser)
-    print("Effective device MAC address: " + ':'.join('{:02x}'.format(x) for x in mac))
-
-    if args.action == "write":
-        writeFirmware(ser, args.file)
-    if args.action == "read":
-        readFirmware(ser, args.file)
-    if args.action == "verify":
-        verifyFirmware(ser, args.file)
+    # Create the flasher object
+    flasher = Flasher(ser, args.verbose)
+    flasher.run(args.action, args.file)
 
 
-main()
+if __name__ == "__main__":
+    main()
