8111a372249192f07dca28cec37f628afce0cb5f
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2024-03-31 17:47:47 +0300

    Add emulation of EEPROM reading and writing commands

--- jn51xx_emulator.py
+++ jn51xx_emulator.py
@@ -9,7 +9,11 @@ class Emulator:
         self.ser = serial.Serial(port, baudrate=38400, timeout=1)
         self.verbose = verbose                                 
 
-        self.flash_data = [0xff] * (512 * 1024)  # 512KB of flash memory (JN5169)
+        self.flash_data = [0xff] * (512 * 1024)         # 512KB of flash memory (JN5169)
+        self.flash_start_addr = 0x00000000
+        self.eeprom_data = [0x00] * (16 * 1024 - 64)    # 16k - 64 bytes of EEPROM memory (JN5169). Final 64-byte segment is unusable
+        self.ram_data = [0x00] * (32 * 1024)            # 32k of RAM (JN5169)
+        self.ram_start_addr = 0x04000000
 
 
     def sendResponse(self, msgtype, data):
@@ -30,31 +34,6 @@ class Emulator:
         bootloaderVer = 0x000b0002
         resp = struct.pack('>BII', 0, chipID, bootloaderVer)
         self.sendResponse(GET_CHIP_ID_RESPONSE, resp)
-
-
-    def emulateRAMData(self, addr, len):
-        if addr == MEMORY_CONFIG_ADDRESS:
-            return struct.pack(">IIII", 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)
-
-        if addr == FACTORY_MAC_ADDRESS:
-            return struct.pack("<BBBBBBBB", 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88)
-        
-        if addr == OVERRIDEN_MAC_ADDRESS:
-            return struct.pack("<BBBBBBBB", 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff)
-        
-        if addr == CHIP_SETTINGS_ADDRESS:
-            return struct.pack(">IIII", 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)
-        
-        print(f"Warning: Attempt to read {len} bytes at unknown address 0x{addr:08x}")
-        return bytes(len)
-
-
-    def emulateRAMRead(self, req):
-        addr, len = struct.unpack("<IH", req)
-        resp = struct.pack('>B', 0)
-        resp += self.emulateRAMData(addr, len)
-
-        self.sendResponse(RAM_READ_RESPONSE, resp)
 
 
     def emulateSelectFlashType(self, req):
@@ -99,33 +78,83 @@ class Emulator:
             print("Warning: Changing the baud rate is not supported")
 
 
-    def emulateRAMWrite(self, req):
-        addr = struct.unpack("<I", req[0:4])
-        data = req[4:]
+    def emulateReadSpecialRegisters(self, addr, len):
+        if addr == MEMORY_CONFIG_ADDRESS:
+            return struct.pack(">IIII", 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)
 
-        # TODO: store data in memory
+        if addr == FACTORY_MAC_ADDRESS:
+            return struct.pack("<BBBBBBBB", 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88)
+        
+        if addr == OVERRIDEN_MAC_ADDRESS:
+            return struct.pack("<BBBBBBBB", 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff)
+        
+        if addr == CHIP_SETTINGS_ADDRESS:
+            return struct.pack(">IIII", 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)
+        
+        print(f"Warning: Attempt to read {len} bytes at unknown address 0x{addr:08x}")
+        return bytes(len)
+
+
+    def emulateMemoryRead(self, req, memory, start_addr = 0):
+        addr, len = struct.unpack("<IH", req)
+        data = memory[addr - start_addr : addr - start_addr + len]
 
         resp = struct.pack("<B", 0)
-        self.sendResponse(RAM_WRITE_RESPONSE, resp)
+        resp += bytes(data)
+        return resp
 
 
-    def emulateFlashWrite(self, req):
+    def emulateMemoryWrite(self, req, memory, start_addr = 0):
         addr = struct.unpack("<I", req[0:4])[0]
         data = req[4:]
 
-        self.flash_data[addr : addr + len(data)] = data
+        memory[addr - start_addr : addr + len(data) - start_addr] = data
 
         resp = struct.pack("<B", 0)
-        self.sendResponse(FLASH_WRITE_RESPONSE, resp)
+        return resp
+
+
+    def emulateRAMRead(self, req):
+        addr, len = struct.unpack("<IH", req)
+        if addr < self.ram_start_addr:
+            resp = struct.pack("<B", 0)
+            resp += self.emulateReadSpecialRegisters(addr, len)
+        else:
+            resp = self.emulateMemoryRead(req, self.ram_data, self.ram_start_addr)
+        self.sendResponse(RAM_READ_RESPONSE, resp)
+
+
+    def emulateRAMWrite(self, req):
+        resp = self.emulateMemoryWrite(req, self.ram_data, self.ram_start_addr)
+        self.sendResponse(RAM_WRITE_RESPONSE, resp)
 
 
     def emulateFlashRead(self, req):
-        addr, len = struct.unpack("<IH", req)
-        data = bytes(self.flash_data[addr : addr + len])
+        resp = self.emulateMemoryRead(req, self.flash_data)
+        self.sendResponse(FLASH_READ_RESPONSE, resp)
+
+
+    def emulateFlashWrite(self, req):
+        resp = self.emulateMemoryWrite(req, self.flash_data)
+        self.sendResponse(FLASH_WRITE_RESPONSE, resp)
+
+
+    def emulateEEPROMRead(self, req):
+        resp = self.emulateMemoryRead(req, self.eeprom_data)
+        self.sendResponse(EEPROM_READ_RESPONSE, resp)
+
+
+    def emulateEEPROMWrite(self, req):
+        resp = self.emulateMemoryWrite(req, self.eeprom_data)
+        self.sendResponse(EEPROM_WRITE_RESPONSE, resp)
+
+
+    def emulateRun(self, req):
+        addr = struct.unpack("<I", req)[0]
+        print(f"Emulating running code at address 0x{addr:08x}")
 
         resp = struct.pack("<B", 0)
-        resp += data
-        self.sendResponse(FLASH_READ_RESPONSE, resp)
+        self.sendResponse(RUN_RESPONSE, resp)
 
 
     def run(self):
@@ -144,8 +173,6 @@ class Emulator:
             data = data[:-1]
             if msgtype == GET_CHIP_ID_REQUEST:
                 self.emulateGetChipId(data)
-            elif msgtype == RAM_READ_REQUEST:
-                self.emulateRAMRead(data)
             elif msgtype == SELECT_FLASH_TYPE_REQUEST:
                 self.emulateSelectFlashType(data)
             elif msgtype == CHANGE_BAUD_RATE_REQUEST:
@@ -154,12 +181,20 @@ class Emulator:
                 self.emulateFlashErase(data)
             elif msgtype == RESET_REQUEST:
                 self.emulateReset(data)
+            elif msgtype == RAM_READ_REQUEST:
+                self.emulateRAMRead(data)
             elif msgtype == RAM_WRITE_REQUEST:
                 self.emulateRAMWrite(data)
-            elif msgtype == FLASH_WRITE_REQUEST:
-                self.emulateFlashWrite(data)
             elif msgtype == FLASH_READ_REQUEST:
                 self.emulateFlashRead(data)
+            elif msgtype == FLASH_WRITE_REQUEST:
+                self.emulateFlashWrite(data)
+            elif msgtype == EEPROM_READ_REQUEST:
+                self.emulateEEPROMRead(data)
+            elif msgtype == EEPROM_WRITE_REQUEST:
+                self.emulateEEPROMWrite(data)
+            elif msgtype == RUN_REQUEST:
+                self.emulateRun(data)
             else:
                 print(f"Unsupported message type: {msgtype:02x}")
 
